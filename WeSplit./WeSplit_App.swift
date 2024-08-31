34013ca8562c3494a1893939fe35670804b99583
JoshuaRosado <joshua06rosado@gmail.com>
2024-08-31 15:37:06 -0500

    Initial Commit

---
+++ WeSplit./WeSplit_App.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplit_App.swift
+//  WeSplit.
+//
+//  Created by Joshua Rosado Olivencia on 8/31/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplit_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
